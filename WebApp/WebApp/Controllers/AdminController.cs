67cdc271cde6c787e9877cd472b00cddf6978016
Mikolaj <mikolaj.mikolaj@gmail.com>
2020-05-02 19:50:02 +0200

    admin może zmienić hasło lekarza i wypisać go z systemu. Administrator może przeglądać dane wszystkich pacjentów i dokonywać na nich takich samych akcji jak lekarze

---
+++ WebApp/WebApp/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+	[Route("Admin")]
+    public class AdminController : Controller
+    {
+	    private readonly ApplicationDbContext _db;
+
+	    public AdminController(ApplicationDbContext db)
+	    {
+		    _db = db;
+	    }
+
+	    [HttpGet("Patients")]
+        public IActionResult Patients()
+        {
+	        ViewBag.Patients = _db.Patient.Where(p => p.NotPatientAnymore == false).ToList();
+	        ViewBag.Doctors = _db.User.Where(u => u.IsAdmin == false).ToList();
+	        ViewBag.SharedPatients = _db.SharedPatients.ToList();
+            return View();
+        }
+    }
+}
